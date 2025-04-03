3c42a36ccaea594e19de48137dbc658e1f9398c6
BKPham90 <brian.khoa.pham@gmail.com>
2025-04-03 18:53:21 -0500

    Implemented tkinter window and displayed queried data to it.

--- Project1.py
+++ Project1.py
@@ -5,23 +5,42 @@ from tkinter import *
 connection = sqlite3.connect("GA_Maintenance.db")
 cursor = connection.cursor()
 
-createFile = open("create.sql", "r")
-createCommands = createFile.read().split(';')
-createFile.close()
-for createCommand in createCommands:
-    cursor.execute(createCommand)
-    connection.commit()
+# createFile = open("create.sql", "r")
+# createCommands = createFile.read().split(';')
+# createFile.close()
+# for createCommand in createCommands:
+#     cursor.execute(createCommand)
+#     connection.commit()
 
-insertFile = open("insert.sql", "r")
-insertCommands = insertFile.read().split(';')
-insertFile.close()
-for insertCommand in insertCommands:
-    cursor.execute(insertCommand)
-    connection.commit()
+# insertFile = open("insert.sql", "r")
+# insertCommands = insertFile.read().split(';')
+# insertFile.close()
+# for insertCommand in insertCommands:
+#     cursor.execute(insertCommand)
+#     connection.commit()
 
-crudFile = open("crud.sql", "r")
-crudCommands = crudFile.read().split(';')
-crudFile.close()
-for crudCommand in crudCommands:
-    cursor.execute(crudCommand)
-    connection.commit()
+window = Tk()
+window.title("General Aviation Maintence")
+
+dataTextBox = Text(window)
+dataTextBox.pack()
+
+def displayData():
+    cursor.execute("""
+                   SELECT *
+                   FROM Airplanes
+                   ORDER BY tail_number
+                   """
+    )
+    results = cursor.fetchall()
+    for row in results:
+        for column in row:
+            dataTextBox.insert(END, column)
+            dataTextBox.insert(END, "\t")
+        dataTextBox.insert(END, "\n")
+
+displayData()
+
+window.mainloop()
+
+connection.close()
